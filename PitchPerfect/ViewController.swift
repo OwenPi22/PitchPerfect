5b95e46934325f9e2b4f918a612e599c0ef39d85
Zhiqiang Pi <zhpi@ucsd.edu>
2019-08-23 21:42:05 +0800

    Images as buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        print("Run")
+        
     }
 
     override func didReceiveMemoryWarning() {
